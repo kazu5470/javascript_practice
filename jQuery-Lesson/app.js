bf1cafb13603cd248fb245944b25723393739a46
kazu5470 <105265063+kazu5470@users.noreply.github.com>
2022-09-05 12:48:13 +0000

    JS6章

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,9 +1,20 @@
+// $(function(){
+//     $('.box1').slideDown(1000,function(){
+//         $('.box1').css({
+//             'background-color': '#0000FF',
+//             'width': '200px',
+//             'height': '100px'
+//         }).slideUp(2000);
+//     });
+// });
+
 $(function(){
-    $('.box1').slideDown(1000,function(){
-        $('.box1').css({
-            'background-color': '#0000FF',
-            'width': '200px',
-            'height': '100px'
-        }).slideUp(2000);
+    $('.box1').on('click', function(){
+        $('.box1').addClass('box1-ext');
+    });
+});
+$(function(){
+    $('.box1').mouseout(function(){
+        $('.box1').removeClass('box1-ext');
     });
 });
